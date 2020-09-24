9c4aa3e17d0b98e27e6463b9792682c5bb02b038
Andreas Wass <andreas.wass@dalelven.com>
2020-09-24 14:20:32 +0200

    Work work work

--- examples/timer.rs
+++ examples/timer.rs
@@ -1,63 +1,98 @@
 use embedded_async::timer::*;
-use std::sync::{Mutex, Arc};
-use embedded_time::duration::Microseconds;
+use std::sync::{Mutex, Arc, Condvar};
+use embedded_time::duration::{Microseconds, Seconds};
 use std::task::Waker;
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicU32, Ordering};
 use std::time::Duration;
 use std::thread::JoinHandle;
 
-#[derive(PartialOrd, PartialEq)]
-enum  TimerState {
+#[derive(PartialOrd, PartialEq, Debug, Copy, Clone)]
+enum TimerState {
     Started(u32),
     Paused,
     Stopped,
 }
 
-struct ThreadTimer {
+struct Inner {
+    cv: Condvar,
     state: Mutex<TimerState>,
     waker: Mutex<Option<Waker>>,
     elapsed: std::sync::atomic::AtomicU32,
+    is_running: AtomicBool,
+}
+
+struct ThreadTimer {
+    state: Arc<Inner>
 }
 
 impl ThreadTimer {
-    pub fn new() -> (Arc<Self>, JoinHandle<()>) {
-        let retval = Arc::new(Self {
-            state: Mutex::new(TimerState::Paused),
-            waker: Mutex::new(None),
-            elapsed: AtomicU32::new(0),
-        });
+    pub fn new() -> (Self, JoinHandle<()>) {
+        let retval = Self {
+            state: Arc::new(Inner {
+                cv: Condvar::new(),
+                state: Mutex::new(TimerState::Paused),
+                waker: Mutex::new(None),
+                elapsed: AtomicU32::new(0),
+                is_running: AtomicBool::new(false),
+            })
+        };
 
-        let thread_data = retval.clone();
+        let thread_data = retval.state.clone();
 
         let joiner = std::thread::spawn(move || {
-            let mut start_time : std::time::Instant;
             loop {
-                while *thread_data.state.lock().unwrap() == TimerState::Paused {
-                    std::thread::sleep(Duration::from_micros(10));
-                }
+                let pause_timer = || {
+                    *thread_data.state.lock().unwrap() = TimerState::Paused;
+                };
+
+                thread_data.is_running.store(false, Ordering::Release);
+
+                let current_state =
+                {
+                    let lock = thread_data.state.lock().unwrap();
+                    *thread_data.cv.wait_while(lock, |x| {
+                        match *x {
+                            TimerState::Paused => true,
+                            _ => false,
+                        }
+                    }).unwrap()
+                };
 
                 thread_data.elapsed.store(0, Ordering::Release);
+                thread_data.is_running.store(true, Ordering::Release);
 
-                if let TimerState::Started(x) = *thread_data.state.lock().unwrap() {
-                    start_time = std::time::Instant::now();
+                if let TimerState::Started(x) = current_state {
+                    let start_time = std::time::Instant::now();
                     let stop_time = start_time + Duration::from_micros(x as u64);
-                    while let TimerState::Started(_) = *thread_data.state.lock().unwrap() {
-                        std::thread::sleep(Duration::from_micros(10));
-                        if std::time::Instant::now() >= stop_time {
 
-                            let mut waker = thread_data.waker.lock().unwrap();
-                            if let Some(x) = (*waker).take() {
-                                x.wake();
+                    let timeout = {
+                        let guard = thread_data.state.lock().unwrap();
+                        let res = thread_data.cv.wait_timeout_while(guard,
+                            Duration::from_micros(x.into()), |x| {
+                            if let TimerState::Started(_) = *x {
+                                true
+                            } else {
+                                false
                             }
-                            break;
+                        });
+
+                        res.unwrap().1.timed_out()
+                    };
+
+                    if timeout {
+                        let mut waker = thread_data.waker.lock().unwrap();
+                        if let Some(x) = (*waker).take() {
+                            x.wake();
                         }
                     }
 
                     let elapsed = std::time::Instant::now() - start_time;
                     thread_data.elapsed.store(elapsed.as_micros() as u32, Ordering::Release);
+
+                    pause_timer();
                 }
 
-                if *thread_data.state.lock().unwrap() == TimerState::Stopped {
+                if current_state == TimerState::Stopped {
                     break;
                 }
             }
@@ -67,27 +102,70 @@ impl ThreadTimer {
     }
 
     fn stop(&self) {
-        *self.state.lock().unwrap() = TimerState::Stopped;
+        *self.state.state.lock().unwrap() = TimerState::Stopped;
     }
 }
 
 impl TimerBackend for ThreadTimer {
     fn wake_in(&mut self, waker: Waker, micros: Microseconds<u32>) {
-        *self.waker.lock().unwrap() = Some(waker);
-        *self.state.lock().unwrap() = TimerState::Started(micros.0);
+        *self.state.waker.lock().unwrap() = Some(waker);
+        {
+            *self.state.state.lock().unwrap() = TimerState::Started(micros.0);
+            self.state.cv.notify_one();
+        }
+        while self.state.is_running.load(Ordering::Acquire) == false {}
     }
 
     fn pause(&mut self) {
-        *self.state.lock().unwrap() = TimerState::Paused;
+        {
+            *self.state.state.lock().unwrap() = TimerState::Paused;
+            self.state.cv.notify_one();
+        }
+
+        while self.state.is_running.load(Ordering::Acquire) {}
     }
 
     fn elapsed_time(&self) -> Microseconds<u32> {
-        Microseconds::new(self.elapsed.load(Ordering::Acquire))
+        Microseconds::new(self.state.elapsed.load(Ordering::Acquire))
+    }
+
+    fn reset_elapsed_time(&self) {
+        self.state.elapsed.store(0, Ordering::Release);
     }
 }
 
+async fn hello_world(id: i32, delay: Microseconds, handle: TimerHandle<impl TimerBackend>) {
+    loop {
+        println!("Sleeping {}", id);
+        let sleep_time = std::time::Instant::now();
+        handle.delay(delay).await;
+        let wake_time = std::time::Instant::now();
+        println!("Hello world from {}, slept for {:?}", id, wake_time - sleep_time);
+    }
+}
+
+
 fn main() {
-    let timer = ThreadTimer::new();
-    timer.0.stop();
-    timer.1.join().unwrap();
+    /*let (timer_backend, thread_join) = ThreadTimer::new();
+    let timer = Timer::new(timer_backend);
+    pin_utils::pin_mut!(timer);
+    let (task, handle) = timer.split().unwrap();
+
+    uio::task_start!(timer_task, embedded_async::timer::run_timer(task));
+    uio::task_start!(hello_world1, hello_world(1, Microseconds(1_000_000), handle.clone()));
+    uio::task_start!(hello_world2, hello_world(2, Microseconds(1_500_000), handle));
+    uio::executor::run();
+    thread_join.join();*/
+
+    let mutex = Mutex::new(false);
+    let cv = Condvar::new();
+    loop {
+        let start = std::time::Instant::now();
+        let lock = mutex.lock().unwrap();
+        cv.wait_timeout_while(lock, Duration::from_micros(333_333), |x| {
+            true
+        });
+        let end = std::time::Instant::now();
+        println!("{:?}", end-start);
+    }
 }
--- src/timer/mod.rs
+++ src/timer/mod.rs
@@ -3,7 +3,7 @@ use core::future::Future;
 use core::pin::Pin;
 
 use crate::intrusive::rc::*;
-use crate::intrusive::double_list::List;
+use crate::intrusive::double_list::{List, Link};
 
 use embedded_time::duration::{Microseconds, Extensions};
 use core::hint::unreachable_unchecked;
@@ -24,18 +24,54 @@ pub trait TimerBackend {
     ///
     /// The value is only required to be valid when the timer is paused.
     fn elapsed_time(&self) -> Microseconds;
+    /// Reset the elapsed time since start.
+    ///
+    /// This function is only valid while the timer is paused and `elapsed_time` after this must
+    /// return 0.
+    fn reset_elapsed_time(&self);
 }
 
-struct WaitingTimers {
+struct WaitingTimer {
     time_until_timeout: Microseconds,
-    waker: Waker,
+    waker: Option<Waker>,
 }
 
 pub struct Timer<Timer: TimerBackend> {
     backend: Timer,
     waker: Option<Waker>,
-    sleeping_timers: List<WaitingTimers>,
-    new_timers: List<WaitingTimers>,
+    sleeping_timers: List<WaitingTimer>,
+    new_timers: List<WaitingTimer>,
+    rc: RcAnchor<*mut Self>,
+}
+
+impl<T: TimerBackend> Timer<T> {
+    pub fn new(backend: T) -> Self {
+        Self {
+            backend,
+            waker: None,
+            sleeping_timers: List::new(),
+            new_timers: List::new(),
+            rc: RcAnchor::new(core::ptr::null_mut()),
+        }
+    }
+
+    pub fn split(self: Pin<&mut Self>) -> Option<(TimerTask<T>, TimerHandle<T>)> {
+        if (*self.rc).is_null() {
+            let me = unsafe { self.get_unchecked_mut() };
+            me.rc = RcAnchor::new(me);
+            let rc_ref = unsafe { me.rc.get_ref() };
+            Some(
+                (TimerTask {
+                    timer: RcRef::clone(&rc_ref),
+                },
+                 TimerHandle {
+                     timer: rc_ref
+                 })
+            )
+        } else {
+            None
+        }
+    }
 }
 
 pub struct TimerTask<T: TimerBackend> {
@@ -48,7 +84,7 @@ struct TimerTaskRunner<'a, T: TimerBackend> {
 
 impl<T: TimerBackend> TimerTaskRunner<'_, T> {
     fn update_sleeping_timers(&mut self, elapsed: Microseconds) -> Option<Microseconds> {
-        let mut sleepers = List::<WaitingTimers>::new();
+        let mut sleepers = List::<WaitingTimer>::new();
         self.timer.sleeping_timers.move_to(&mut sleepers);
         let mut next_wait_time: Option<Microseconds> = None;
 
@@ -63,17 +99,39 @@ impl<T: TimerBackend> TimerTaskRunner<'_, T> {
                     next_wait_time = next_wait_time.and_then(|cur_min| {
                         Some(cur_min.min(owner.time_until_timeout))
                     }).or(Some(owner.time_until_timeout));
+                } else {
+                    owner.waker.take().expect("").wake();
                 }
-                else {
-                    owner.waker.wake_by_ref();
-                }
-            }
-            else {
+            } else {
                 unsafe { unreachable_unchecked() };
             }
         }
 
         next_wait_time
+    }
+
+    fn append_new_timers(&mut self) -> Option<Microseconds> {
+        let mut to_append = List::<WaitingTimer>::new();
+        self.timer.new_timers.move_to(&mut to_append);
+
+        let mut retval: Option<Microseconds> = None;
+
+        while let Some(n) = to_append.pop() {
+            let x_ptr = n as *mut _;
+
+            if let Some(owner) = n.owner_mut() {
+                retval = retval.and_then(|w| {
+                    Some(w.min(owner.time_until_timeout))
+                }).or(Some(owner.time_until_timeout));
+                unsafe {
+                    self.timer.sleeping_timers.push(owner, x_ptr);
+                }
+            } else {
+                unsafe { unreachable_unchecked() };
+            }
+        }
+
+        retval
     }
 }
 
@@ -87,12 +145,12 @@ impl<T: TimerBackend> Future for TimerTaskRunner<'_, T> {
         me.timer.waker = Some(cx.waker().clone());
         me.timer.backend.pause();
         let elapsed = me.timer.backend.elapsed_time();
+        me.timer.backend.reset_elapsed_time();
         let mut next_wait_time: Option<Microseconds> = None;
 
         if elapsed.0 > 0 {
             next_wait_time = me.update_sleeping_timers(elapsed);
-        }
-        else {
+        } else {
             unsafe {
                 for x in me.timer.sleeping_timers.iter() {
                     next_wait_time = next_wait_time.and_then(|w| {
@@ -102,24 +160,14 @@ impl<T: TimerBackend> Future for TimerTaskRunner<'_, T> {
             }
         }
 
-        let mut to_append = List::<WaitingTimers>::new();
-        me.timer.new_timers.move_to(&mut to_append);
+        let new_timers_min_timeout = me.append_new_timers();
 
-        while let Some(n) = to_append.pop() {
-            let x_ptr = n as *mut _;
-
-            if let Some(owner) = n.owner_mut() {
-                next_wait_time = next_wait_time.and_then(|w| {
-                    Some(w.min(owner.time_until_timeout))
-                }).or(Some(owner.time_until_timeout));
-                unsafe {
-                    me.timer.sleeping_timers.push(owner, x_ptr);
-                }
-            }
-            else {
-                unsafe { unreachable_unchecked() };
-            }
-        }
+        let next_wait_time = match (next_wait_time, new_timers_min_timeout) {
+            (None, None) => None,
+            (Some(x), Some(y)) => Some(x.min(y)),
+            (x, None) => x,
+            (None, y) => y,
+        };
 
         if let Some(next_wait_time) = next_wait_time {
             me.timer.backend.wake_in(cx.waker().clone(), next_wait_time);
@@ -127,6 +175,61 @@ impl<T: TimerBackend> Future for TimerTaskRunner<'_, T> {
 
         // Always poll pending, this future will never complete!
         Poll::Pending
+    }
+}
+
+pub struct TimerHandle<T: TimerBackend> {
+    timer: RcRef<*mut Timer<T>>,
+}
+
+impl<T: TimerBackend> TimerHandle<T> {
+    pub fn delay(&self, duration: Microseconds) -> DelayFuture<T> {
+        DelayFuture {
+            timer: RcRef::clone(&self.timer),
+            link: Link::new(),
+            waiter: WaitingTimer {
+                waker: None,
+                time_until_timeout: duration,
+            }
+        }
+    }
+}
+
+impl<T: TimerBackend> Clone for TimerHandle<T> {
+    fn clone(&self) -> Self {
+        Self {
+            timer: RcRef::clone(&self.timer)
+        }
+    }
+}
+
+pub struct DelayFuture<T: TimerBackend> {
+    timer: RcRef<*mut Timer<T>>,
+    waiter: WaitingTimer,
+    link: Link<WaitingTimer>,
+}
+
+impl<T: TimerBackend> Future for DelayFuture<T>
+{
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.waiter.waker.is_some() {
+            Poll::Pending
+        }
+        else if self.waiter.time_until_timeout > 0u32.microseconds() {
+            let me = unsafe { self.get_unchecked_mut() };
+            let timer = unsafe { &mut **me.timer };
+            unsafe { timer.new_timers.push(&mut me.waiter, &mut me.link) };
+            me.waiter.waker = Some(cx.waker().clone());
+            if let Some(x) = &timer.waker {
+                x.wake_by_ref();
+            }
+
+            Poll::Pending
+        } else {
+            Poll::Ready(())
+        }
     }
 }
 
